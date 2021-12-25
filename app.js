f3bae235357ad2ca7d7ec8cd164970603f488fab
MEDA ANIL KUMAR <medaanilkumar1998@gmail.com>
2021-12-25 10:06:56 +0530

    done

---
+++ app.js
@@ -0,0 +1,13 @@
+first=document.querySelector('#first')
+but=document.querySelector('button')
+output=document.querySelector('#second')
+but.addEventListener('click',translate)
+
+function translate(){
+    url='https://api.funtranslations.com/translate/minion.json'+"?"+"text="+first.value
+    console.log(url)
+    fetch(url)
+    .then(response=>response.json())
+    .then(data=>output.value=data.contents.translated)
+    .catch(console.error("server is busy");)
+}
